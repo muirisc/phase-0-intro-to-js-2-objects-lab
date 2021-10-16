e46ac5a1c7b72c0ae818d1282db2ac78c46415ed
muirisc <91299876+muirisc@users.noreply.github.com>
2021-10-15 22:58:53 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const employee = {name: "Dale",
+     streetAddress: "123 Street" };
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newemployee = {...employee };
+newemployee[key] = value;
+return newemployee;
+}
+
+function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value ;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newemployee = {...employee };
+    delete newemployee[key]; 
+    return newemployee;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee [key];
+    return employee;
+}
